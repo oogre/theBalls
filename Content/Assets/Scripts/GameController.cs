275a4cb45580633e4c0ac33eb68efe74d5429d4a
oogre <vincent@ogre.be>
2020-01-02 22:55:13 +0100

    gameManager

--- Content/Assets/Scripts/GameController.cs
+++ Content/Assets/Scripts/GameController.cs
@@ -4,23 +4,11 @@ using UnityEngine;
 
 public abstract class DragController : MonoBehaviour
 {
-    public float playerDepth;
-    protected Vector3 offset;
-    public Vector3 force;
-
     public bool dragging;
-    public bool WasDragging;
-
     public abstract void OnMouseDragStart();
     public abstract void OnMouseDragEnd();
     public abstract void OnMouseDragging();
 
-    public void Start(){
-        //translate the player position from the world to Screen Point
-        playerDepth = Camera.main.WorldToScreenPoint(transform.localPosition).z;
-        //calculate any difference between the cubes world position and the mouses Screen position converted to a world point  
-        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, playerDepth));
-    }
     void OnMouseDrag()
     {
         if (!dragging) {
@@ -42,15 +30,17 @@ public abstract class DragController : MonoBehaviour
 
 public class GameController : DragController
 {
+    private Rigidbody rb;
+    private float playerDepth;
+    private GameObject forceArrow;
 
+    public Vector3 force;
     public float forceMultiplier = 1000;
     public GameObject forceArrowPrefab;
-    GameObject forceArrow;
-    Rigidbody rb;
-
-    new void Start()
+    
+    void Start()
     {
-        base.Start();
+        playerDepth = Camera.main.WorldToScreenPoint(transform.localPosition).z;
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
@@ -68,8 +58,9 @@ public class GameController : DragController
     public override void OnMouseDragging() {
         Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, playerDepth);
         //convert the screen mouse position to world point and adjust with offset
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace);
         force = transform.position - curPosition;
         forceArrow.transform.localScale = force;
+        forceArrow.transform.localPosition = transform.position + .5f * force;
     }
 }
