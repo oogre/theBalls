199da05da1d6fb88e53f40b57088d2255bb6db64
oogre <vincent@ogre.be>
2020-01-03 15:45:43 +0100

    setup camera position

--- Content/Assets/Scripts/CameraFollow2D.cs
+++ Content/Assets/Scripts/CameraFollow2D.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class CameraFollow2D : MonoBehaviour
 {
-    public float FollowSpeed = 2f;
-    public Transform Target;
+	[Range(-10, 10)]
+	public float xOffset = 0;
+	public float FollowSpeed = 2f;
+	public Transform Target;
 
-    private void Update()
-    {
-        Vector3 newPosition = Target.position;
-        newPosition.z = -10;
-        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
-    }
+	private void Start()
+	{
+		transform.position = getCamNewPos();
+	}
+
+	private void Update()
+	{
+		transform.position = Vector3.Slerp(transform.position, getCamNewPos(), FollowSpeed * Time.deltaTime);
+	}
+
+	private Vector3 getCamNewPos() {
+		Vector3 newPosition = Target.position;
+		newPosition.z -= 10;
+		newPosition.x += xOffset;
+		return newPosition;
+	}
 }
