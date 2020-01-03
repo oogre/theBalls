5a632a07ea55560598fb49d4da279a1288db8bd9
rulg01 <49067441+rulg01@users.noreply.github.com>
2020-01-03 02:51:36 +0100

    add and deleter shit

---
+++ Content/Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public Transform Target;
+
+    private void Update()
+    {
+        Vector3 newPosition = Target.position;
+        newPosition.z = -10;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+    }
+}
